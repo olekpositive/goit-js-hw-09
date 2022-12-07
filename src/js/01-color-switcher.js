e620d7bdd8bb77cc38075f3f51cec13b3c4da990
Oleksandr Kihim <oleksandr.kigim@gmail.com>
2022-12-07 19:56:24 +0100

    Added_color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,29 @@
+
+
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let timer;
+
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+btnStart.addEventListener("click", () => {
+    btnStart.setAttribute("disabled", true);
+    btnStop.removeAttribute("disabled");
+    timer = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+});
+
+
+
+btnStop.addEventListener("click", () => {
+    btnStart.removeAttribute("disabled"); 
+    btnStop.setAttribute("disabled", true);
+    clearInterval(timer);
+}
+);
