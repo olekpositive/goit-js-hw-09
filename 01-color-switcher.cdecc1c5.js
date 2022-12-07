1a8cd4018c1fadd314be25cf853009d235554710
olekpositive <103032515+olekpositive@users.noreply.github.com>
2022-12-07 18:59:38 +0000

    Deploying to gh-pages from @ olekpositive/goit-js-hw-09@e620d7bdd8bb77cc38075f3f51cec13b3c4da990 🚀

---
+++ 01-color-switcher.cdecc1c5.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let d;t.addEventListener("click",(()=>{t.setAttribute("disabled",!0),e.removeAttribute("disabled"),d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(()=>{t.removeAttribute("disabled"),e.setAttribute("disabled",!0),clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.cdecc1c5.js.map
