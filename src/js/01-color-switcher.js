61dd318dd9c9737afeff6d308ef67227ae37af3b
Oleksandr Kihim <oleksandr.kigim@gmail.com>
2022-12-07 17:02:45 +0100

    Update_1.2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,25 +2,3 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
-
-
-const body = document.querySelector("body");
-const color = document.querySelector("span.color");
-
-
-const getRandomHexColor = () => {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0).toUpperCase();
-    document.body.style.backgroundColor = "#" + randomColor;
-    color.innerHTML = "#" + randomColor;
-}
-
-const button = document.querySelector("button.change-color");
-button.addEventListener("click", getRandomHexColor);
-
-
-window.addEventListener("load", () => {
-    const hexColor = "#FFFFFF";
-    color.textContent = hexColor.toUpperCase();
-    body.style.backgroundColor = hexColor;
-});
