36e118b7b4a89192f5075c92c901504a7a10736a
Oleksandr Kihim <oleksandr.kigim@gmail.com>
2022-12-11 15:00:41 +0100

    Update_3.1_task_#1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,7 @@ const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 
+btnStop.setAttribute("disabled", true);
 let timer;
 
 function getRandomHexColor() {
